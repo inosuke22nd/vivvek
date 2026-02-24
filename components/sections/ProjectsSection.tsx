3043ceee10869c6d6cd514612054e7544e1ef247
inosuke22nd <inosuke22nd6@gmail.com>
2026-02-24 23:20:02 +0530

    Fix spelling of 'Learntube' and 'Greatlearning'

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -61,9 +61,9 @@ const fadeInUp: Variants = {
 const certificates = [
   { title: "SparkMinda", image: "/certificates/Sparkminda.jpg" },
   { title: "Almonda", image: "/certificates/Almonda.jpg" },
-  { title: "LearnTube", image: "/certificates/Learntube.jpg" },
+  { title: "Learntube", image: "/certificates/Learntube.jpg" },
   { title: "Google", image: "/certificates/Google.jpg" },
-  { title: "GreatLearning", image: "/certificates/Greatlearning.jpg" },
+  { title: "Greatlearning", image: "/certificates/Greatlearning.jpg" },
 ];
 
 /* -------------------- COMPONENT -------------------- */
